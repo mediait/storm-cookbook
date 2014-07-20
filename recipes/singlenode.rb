671f2efd7bccb90817f7accada698f0a85d619f3
Yury Egorenkov <yury.egorenkov@gmail.com>
2014-07-20 19:05:49 +0400

    Refactoring

--- recipes/singlenode.rb
+++ recipes/singlenode.rb
@@ -1,5 +1,7 @@
 include_recipe "storm"
 
+storm_path = "/#{node[:storm][:deploy][:user]}/apache-storm-#{node[:storm][:version]}"
+
 template "Storm conf file" do
   path "/#{node[:storm][:deploy][:user]}/apache-storm-#{node[:storm][:version]}/conf/storm.yaml"
   source "singlenode.yaml.erb"
@@ -8,56 +10,28 @@ template "Storm conf file" do
   mode 0644
 end
 
+file "/#{node[:storm][:deploy][:user]}/apache-storm-#{node[:storm][:version]}/lib/netty-3.6.3.Final.jar" do
+  action :delete
+end
+
 # Update netty to 3.9.2
 remote_file "/#{node[:storm][:deploy][:user]}/apache-storm-#{node[:storm][:version]}/lib/netty-3.9.2.Final.jar" do
   source "http://central.maven.org/maven2/io/netty/netty/3.9.2.Final/netty-3.9.2.Final.jar"
   action :create_if_missing
 end
 
-file "/#{node[:storm][:deploy][:user]}/apache-storm-#{node[:storm][:version]}/lib/netty-3.6.3.Final.jar" do
-  action :delete
-end
+['nimbus', 'supervisor', 'drpc', 'ui'].each do |service|
+  package = "backtype.storm.daemon.#{service}"
+  package = 'backtype.storm.ui.core' if service == 'ui'
 
-bash "Start nimbus" do
-  user node[:storm][:deploy][:user]
-  cwd "/#{node[:storm][:deploy][:user]}"
-  code <<-EOH
-  pid=$(pgrep -f backtype.storm.daemon.nimbus)
-  if [ -z $pid ]; then
-    nohup apache-storm-#{node[:storm][:version]}/bin/storm nimbus >>nimbus.log 2>&1 &
-  fi
-  EOH
-end
-
-bash "Start supervisor" do
-  user node[:storm][:deploy][:user]
-  cwd "/#{node[:storm][:deploy][:user]}"
-  code <<-EOH
-  pid=$(pgrep -f backtype.storm.daemon.supervisor)
-  if [ -z $pid ]; then
-    nohup apache-storm-#{node[:storm][:version]}/bin/storm supervisor >>supervisor.log 2>&1 &
-  fi
-  EOH
-end
-
-bash "Start DRPC" do
-  user node[:storm][:deploy][:user]
-  cwd "/#{node[:storm][:deploy][:user]}"
-  code <<-EOH
-  pid=$(pgrep -f backtype.storm.daemon.drpc)
-  if [ -z $pid ]; then
-    nohup apache-storm-#{node[:storm][:version]}/bin/storm drpc >>drpc.log 2>&1 &
-  fi
-  EOH
-end
-
-bash "Start ui" do
-  user node[:storm][:deploy][:user]
-  cwd "/#{node[:storm][:deploy][:user]}"
-  code <<-EOH
-  pid=$(pgrep -f backtype.storm.ui.core)
-  if [ -z $pid ]; then
-    nohup apache-storm-#{node[:storm][:version]}/bin/storm ui >>ui.log 2>&1 &
-  fi
-  EOH
+  bash "Start #{service}" do
+    user node[:storm][:deploy][:user]
+    cwd storm_path
+    code <<-EOH
+    pid=$(pgrep -f #{package})
+    if [ -z $pid ]; then
+      ./bin/storm #{service} &
+    fi
+    EOH
+  end
 end

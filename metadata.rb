e59d563b48fffd188808896d79176aa10787cc63
Yury Egorenkov <yury.egorenkov@gmail.com>
2014-08-05 21:13:50 +0400

    Fix dependencies. Extract zookeeper to cookbook.

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,12 @@ license          "MIT License"
 description      "installs and configures Twitter Storm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.2"
+
+%w{
+    debian
+    ubuntu
+}.each do |os|
+  supports os
+end
+
+depends 'zookeeper'
